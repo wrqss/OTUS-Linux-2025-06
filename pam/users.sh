beaac4dbe560e4586cc20e1a009e1120de0ab262
wrqss <s0d@bk.ru>
2025-06-06 22:24:33 +0300

    Update users.sh

--- pam/users.sh
+++ pam/users.sh
@@ -3,4 +3,4 @@
 useradd otusadm && useradd otus
 echo "otusadm:Otus2025!" | chpasswd && echo "otus:Otus2025!" | chpasswd
 groupadd -f admin
-usermod otusadm -a -G admin && usermod root -a -G admin && usermod vagrant -a -G admin
+usermod otusadm -a -G admin && usermod root -a -G admin
