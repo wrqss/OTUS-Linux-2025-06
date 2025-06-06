4f716b6f6844be1375bae0576271162e9d3b765e
wrqss <s0d@bk.ru>
2025-06-06 22:23:09 +0300

    Create users.sh

---
+++ pam/users.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+useradd otusadm && useradd otus
+echo "otusadm:Otus2025!" | chpasswd && echo "otus:Otus2025!" | chpasswd
+groupadd -f admin
+usermod otusadm -a -G admin && usermod root -a -G admin && usermod vagrant -a -G admin
